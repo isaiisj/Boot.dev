54f6a5f1e1009a8f046acb71de7db0adb7d37ba6
Isai <47548422+isaiisj@users.noreply.github.com>
2025-08-22 01:28:06 -0600

    Create 1.find_minimum.py

---
+++ BackendPath-TypeScript/9.LearnDataStructuresAndAlgorithms/1.CH1_AlgorithmsIntro/1.find_minimum.py
@@ -0,0 +1,42 @@
+'''
+
+As mentioned, an "algorithm" is just a set of instructions that can be carried out to solve a problem.
+
+It's not anymore magical than that.
+
+Assignment
+In this course, we'll be building pieces of a pretend social network: LockedIn. 
+LockedIn is a place for professionals to virtue signal about how all the for-profit work they do is actually an altruistic endeavor.
+Think Facebook meets a job fair. It includes tools for influencers to track their growth.
+
+We need to show our users the accounts they follow with the lowest follower counts.
+This will help them know who they follow that isn't popular enough to be worth following anymore.
+
+Implement the "find minimum" algorithm in Python by completing the find_minimum() function. 
+It accepts a list of integers nums and returns the smallest number in the list.
+
+- Set minimum to positive infinity: float("inf").
+- If the list is empty, return None.
+- For each number in the list nums, compare it to minimum. If the number is smaller than minimum, set minimum to that number.
+- minimum is now set to the smallest number in the list. Return it.
+
+'''
+
+def find_minimum(nums):
+
+    # Set minimun to positive infinity
+    min = float('inf')
+
+    # If the list is empty return None
+    if len(nums) == 0:
+        return None
+
+    # For each num in nums
+    for num in nums:
+
+        # If num < min set a new min
+        if num < min:
+            min = num
+
+    # Return min
+    return min
