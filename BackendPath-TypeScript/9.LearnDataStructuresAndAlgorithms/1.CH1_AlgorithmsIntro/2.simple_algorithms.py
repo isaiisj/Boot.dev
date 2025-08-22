1f480f5e93806f007beb8765630f1fbd9325fdf2
Isai <47548422+isaiisj@users.noreply.github.com>
2025-08-22 02:47:20 -0600

    Create 2.simple_algorithms.py

---
+++ BackendPath-TypeScript/9.LearnDataStructuresAndAlgorithms/1.CH1_AlgorithmsIntro/2.simple_algorithms.py
@@ -0,0 +1,24 @@
+'''
+
+For the LockedIn influencer dashboard, we need to calculate the total reach of
+a group of influencers to estimate how many views a post will get if they all share it.
+
+Complete the sum function. It's a slightly modified version of the algorithm above.
+Instead of just two numbers, a and b, it accepts a list of numbers and returns the sum of all of them.
+
+The sum of an empty list should be 0.
+
+'''
+
+
+def sum(nums):
+
+    # Total sum 
+    total = 0
+    
+    # Iterates the list
+    for num in nums:
+        total += num
+
+    # Return total sum
+    return total
